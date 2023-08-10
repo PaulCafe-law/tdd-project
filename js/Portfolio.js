8567f3f9d560cb404458263352bf221b9b53d58f
Jess0430 <e24093126@gs.ncku.edu.tw>
2023-08-10 22:27:51 +0800

    feat: added Bank; refactored Portfolio to use Bank

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -7,15 +7,16 @@ class Portfolio{
     add(...moneys){
         this.moneys = this.moneys.concat(moneys)
     }
-    evaluate(currency){
+    evaluate(bank, currency){
         let failures = []
         let total = this.moneys.reduce((sum,money) => {
-            let convertedAmount = this.convert(money,currency)
-            if(convertedAmount===undefined){
-                failures.push(money.currency+"->"+currency)
-                return sum;
+            try {
+                let convertedMoney = bank.convert(money,currency)
+                return sum+convertedMoney.amount
+            } catch (error) {
+                failures.push(error.message)
+                return sum
             }
-            return sum + convertedAmount
         },0)
         if(!failures.length){
             return new Money(total,currency);
@@ -23,21 +24,7 @@ class Portfolio{
         throw new Error("Missing exchange rate(s):["+failures.join()+"]")
         // failures.join() 是一个数组方法，用于将数组中的所有元素连接成一个字符串，默认使用逗号 , 分隔元素
     }
-    convert(money, currency){
-        let exchangeRates = new Map();
-        exchangeRates.set("EUR->USD", 1.2)
-        exchangeRates.set("USD->KRW", 1100)
-        
-        if(money.currency===currency){
-            return money.amount;
-        }
-        let key = money.currency+"->"+currency;
-        let rate = exchangeRates.get(key)
-        if(rate===undefined){
-            return undefined
-        }
-        return money.amount*rate;
-    }
+    
 }
 
 module.exports = Portfolio;
--- js/test_money.js
+++ js/test_money.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
 const Money = require('./Money');
 const Portfolio = require('./Portfolio');
+const Bank = require('./bank');
 const { type } = require('os');
 
 
 class MoneyTest{
+    constructor(){
+        this.bank = new Bank();
+        this.bank.addExchangeRate("EUR","USD",1.2)
+        this.bank.addExchangeRate("USD","KRW",1100)
+    }
     testMultiplication(){
         let tenEuros = new Money(10,"EUR");
         let twentyEuros = new Money(20,"EUR");
@@ -21,7 +27,7 @@ class MoneyTest{
         let fifteenDollars = new Money(15,"USD")
         let portfolio = new Portfolio();
         portfolio.add(fiveDollars,tenDollars)
-        assert.deepStrictEqual(portfolio.evaluate("USD"),fifteenDollars)
+        assert.deepStrictEqual(portfolio.evaluate(new Bank, "USD"),fifteenDollars)
     }
     testAdditionOfDollarsAndEuros(){
         let fiveDollars = new Money(5,"USD")
@@ -29,7 +35,7 @@ class MoneyTest{
         let portfolio = new Portfolio()
         portfolio.add(fiveDollars,tenEuros)
         let expectedValue = new Money(17,"USD")
-        assert.deepStrictEqual(portfolio.evaluate("USD"),expectedValue)
+        assert.deepStrictEqual(portfolio.evaluate(this.bank, "USD"),expectedValue)
     }
     testAdditionOfDollarsAndWons(){
         let oneDollar = new Money(1,"USD")
@@ -37,7 +43,7 @@ class MoneyTest{
         let portfolio = new Portfolio()
         portfolio.add(oneDollar,elevenHundredWon)
         let expectedValue = new Money(2200,"KRW")
-        assert.deepStrictEqual(portfolio.evaluate("KRW"),expectedValue)
+        assert.deepStrictEqual(portfolio.evaluate(this.bank, "KRW"),expectedValue)
     }
     testAdditionWithMultipleMissingExchangeRates(){
         let oneDollar = new Money(1,"USD")
@@ -48,8 +54,22 @@ class MoneyTest{
         let expectedError = new Error(
             "Missing exchange rate(s):[USD->Kalganid,EUR->Kalganid,KRW->Kalganid]"
         )
-        assert.throws(function(){portfolio.evaluate("Kalganid")}, expectedError)
+        assert.throws(()=>portfolio.evaluate(this.bank, "Kalganid"), expectedError)
         // 表示测试代码中的函数应该抛出一个与 expectedError 匹配的错误，否则测试将失败。
+    }
+    testConversion(){
+        let bank = new Bank()
+        bank.addExchangeRate("EUR","USD",1.2)
+        let tenEuros = new Money(10,"EUR")
+        assert.deepStrictEqual(bank.convert(tenEuros,"USD"), new Money(12,"USD"));
+
+    }
+    testConversionWithMissingExchange(){
+        let bank = new Bank()
+        let tenEuros = new Money(10,"EUR")
+        let expectedError = new Error("EUR->Kalganid")
+        assert.throws(function(){bank.convert(tenEuros, "Kalganid")},
+            expectedError)
     }
     // 一個接一個測試所有方法
     runAllTests(){
